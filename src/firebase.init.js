c41b9d5386eb1a51c9db273ffd2236d8b35a5d75
mohammedshojib <asmdshojib11@gmail.com>
2022-05-05 21:17:19 +0600

    env done

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -2,14 +2,16 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD-0gmaAjUkGPOcKj6oLOZNJCV6ThdbSlQ",
-  authDomain: "car-hunter-11431.firebaseapp.com",
-  projectId: "car-hunter-11431",
-  storageBucket: "car-hunter-11431.appspot.com",
-  messagingSenderId: "812527704773",
-  appId: "1:812527704773:web:a263f16001c621bba0eeac",
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 const app = initializeApp(firebaseConfig);
+
+console.log(apiKey);
 
 export const auth = getAuth(app);
 
